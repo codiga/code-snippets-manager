b299d0ff7d4673baedb25356e27b6d4ff361cfdd
Daniel <daniel@code-inspector.com>
2022-08-19 22:25:36 -0700

    feat: added custom titlebar

--- src/renderer/components/Layout/Layout.tsx
+++ src/renderer/components/Layout/Layout.tsx
@@ -1,13 +1,12 @@
 import { ReactNode } from 'react';
 import { useLocation } from 'react-router-dom';
-import { Box, Button, Flex, Link, useDisclosure } from '@chakra-ui/react';
-import { Avatar, EmptyState } from '@codiga/codiga-components';
+import { Box, Button, Flex, useDisclosure } from '@chakra-ui/react';
+import { EmptyState } from '@codiga/codiga-components';
 
-import { APP_URL } from 'renderer/lib/config';
-import { getAvatarUrl } from 'renderer/utils/userUtils';
 import Login from 'renderer/components/Login';
 import { useUser } from 'renderer/components/UserContext';
 import SideMenu from './SideMenu';
+import Titlebar from './Titlebar';
 
 type LayoutProps = {
   children: ReactNode;
@@ -15,90 +14,63 @@ type LayoutProps = {
 
 export default function Layout({ children }: LayoutProps) {
   const { pathname } = useLocation();
-  const { id, username, accountType } = useUser();
+  const { id } = useUser();
   const { isOpen, onOpen, onClose } = useDisclosure();
 
   return (
-    <Flex
-      flexDirection="column"
-      h="100vh"
-      w="100vw"
-      bg="neutral.25"
-      _dark={{ bg: 'base.dark' }}
-    >
+    <>
       <Flex
-        w="full"
-        justifyContent="flex-end"
-        alignItems="center"
-        gridGap="space_16"
-        pr="space_16"
-        h="40px"
+        flexDirection="column"
+        h="100vh"
+        w="100vw"
+        bg="neutral.25"
+        _dark={{ bg: 'base.dark' }}
       >
-        <Link
-          isExternal
-          href={`${APP_URL}/assistant/snippet/create`}
-          variant="square"
-        >
-          Create Snippet
-        </Link>
+        <Titlebar openLoginModal={onOpen} />
 
-        {id ? (
-          <Avatar
-            name={username || 'Anon'}
-            kind={accountType as any}
-            src={getAvatarUrl({ id, username, accountType })}
-            size="sm"
-          />
-        ) : (
-          // eslint-disable-next-line jsx-a11y/anchor-is-valid
-          <Link as="button" onClick={onOpen} variant="solid" size="sm" p="0">
-            Login
-          </Link>
-        )}
-      </Flex>
-
-      <Flex flex={1}>
-        <Box
-          w="214px"
-          h="full"
-          bg="neutral.25"
-          _dark={{ bg: 'base.dark' }}
-          pt="space_16"
-        >
-          <SideMenu openLoginModal={onOpen} />
-        </Box>
-
-        <Flex
-          flex={1}
-          justifyContent="center"
-          alignItems="flex-start"
-          pt="space_16"
-          bg="neutral.0"
-          _dark={{ bg: 'neutral.100' }}
-        >
-          <Box w="full">
-            {/* IF THE USER'S LOGGED IN, SHOW CONTENT, OTHERWISE SHOW AN LOGIN NOTICE */}
-            {id || pathname === '/' ? (
-              children
-            ) : (
-              <EmptyState
-                title="Log in to access this section"
-                description="Add your API Token to access this section"
-                illustration="disconnected"
-                mt="space_80"
-                mx="auto"
-              >
-                <Button variant="primary" size="sm" onClick={onOpen}>
-                  Add Token
-                </Button>
-              </EmptyState>
-            )}
+        <Flex flex={1}>
+          <Box
+            w="214px"
+            h="full"
+            bg="neutral.25"
+            _dark={{ bg: 'base.dark' }}
+            pt="space_16"
+          >
+            <SideMenu openLoginModal={onOpen} />
           </Box>
-        </Flex>
-      </Flex>
 
-      {/* LOGIN MODAL  */}
-      <Login isOpen={isOpen} closeModal={onClose} />
-    </Flex>
+          <Flex
+            flex={1}
+            justifyContent="center"
+            alignItems="flex-start"
+            pt="space_16"
+            bg="neutral.0"
+            _dark={{ bg: 'neutral.100' }}
+          >
+            <Box w="full">
+              {/* IF THE USER'S LOGGED IN, SHOW CONTENT, OTHERWISE SHOW AN LOGIN NOTICE */}
+              {id || pathname === '/' ? (
+                children
+              ) : (
+                <EmptyState
+                  title="Log in to access this section"
+                  description="Add your API Token to access this section"
+                  illustration="disconnected"
+                  mt="space_80"
+                  mx="auto"
+                >
+                  <Button variant="primary" size="sm" onClick={onOpen}>
+                    Add Token
+                  </Button>
+                </EmptyState>
+              )}
+            </Box>
+          </Flex>
+        </Flex>
+
+        {/* LOGIN MODAL  */}
+        <Login isOpen={isOpen} closeModal={onClose} />
+      </Flex>
+    </>
   );
 }
