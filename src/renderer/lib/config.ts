6b559bc8d8ece8a8f775a0c6780e2c44262f4f37
Daniel <daniel@code-inspector.com>
2022-08-24 06:41:55 -0700

    chore: set prod links

--- src/renderer/lib/config.ts
+++ src/renderer/lib/config.ts
@@ -1,5 +1,10 @@
-export const APP_URL = 'https://app-staging.codiga.io';
-export const API_ROOT_URL = 'https://api-staging.codiga.io';
+const isProd = process.env.NODE_ENV === 'production';
+export const APP_URL = isProd
+  ? 'https://app.codiga.io'
+  : 'https://app-staging.codiga.io';
+export const API_ROOT_URL = isProd
+  ? 'https://api.codiga.io'
+  : 'https://api-staging.codiga.io';
 export const API_URL = `${API_ROOT_URL}/graphql`;
 export const TOKEN = 'electron-token';
 export const CODIGA_THEME = 'codiga-ui-mode';
