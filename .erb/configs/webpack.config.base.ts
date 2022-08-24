03cf8ade5a72411f3e2650bee9116a95f704347e
Daniel <daniel@code-inspector.com>
2022-08-24 07:42:29 -0700

    chore: added process package

--- .erb/configs/webpack.config.base.ts
+++ .erb/configs/webpack.config.base.ts
@@ -45,6 +45,7 @@ const configuration: webpack.Configuration = {
       url: require.resolve('url/'),
       path: require.resolve('path-browserify'),
       buffer: require.resolve('buffer/'),
+      process: require.resolve('process/browser'),
     },
   },
 
