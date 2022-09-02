143c0583e6cc6814af752096b5e074678ffc2365
Daniel <daniel@code-inspector.com>
2022-09-02 10:23:41 -0700

    feat: added snippet/cookbook view pages

---
+++ src/renderer/pages/ViewCookbookSnippets.tsx
@@ -0,0 +1,78 @@
+import { useQuery } from '@apollo/client';
+import { useParams } from 'react-router-dom';
+import { Box, Flex, HStack, Text } from '@chakra-ui/react';
+
+import { GET_COOKBOOK_RECIPES } from '../graphql/queries';
+import { GET_USER_RECIPES_VARIABLES } from '../graphql/variables';
+import ViewCookbookSnippetsError from '../components/ViewCookbookSnippets/ViewCookbookSnippetsError';
+import ViewCookbookSnippetsLoading from '../components/ViewCookbookSnippets/ViewCookbookSnippetsLoading';
+import ViewCookbookSnippetsEmpty from '../components/ViewCookbookSnippets/ViewCookbookSnippetsEmpty';
+import BackButton from '../components/BackButton';
+import FavoriteCookbook from '../components/Favorite/FavoriteCookbook';
+import AvatarAndName from '../components/AvatarAndName';
+import PrivacyAndVotes from '../components/PrivacyAndVotes';
+import FormattedDate from '../components/FormattedDate';
+import SnippetResults from '../components/SnippetResults/SnippetResults';
+
+export default function ViewCookbookSnippets() {
+  const params = useParams();
+
+  const { data, loading, error } = useQuery(GET_COOKBOOK_RECIPES, {
+    variables: {
+      cookbookId: Number(params.cookbookId),
+      ...GET_USER_RECIPES_VARIABLES,
+    },
+  });
+
+  const cookbook = data?.cookbook;
+
+  if (loading) {
+    return <ViewCookbookSnippetsLoading />;
+  }
+
+  if (error || !cookbook) {
+    return <ViewCookbookSnippetsError />;
+  }
+
+  return (
+    <Box h="full">
+      {/* INFO SECTION */}
+      <HStack
+        alignItems="center"
+        bg="neutral.25"
+        _dark={{ bg: 'base.dark' }}
+        h="74px"
+        w="full"
+        spacing="space_16"
+      >
+        <BackButton />
+
+        <Flex alignItems="center" gridGap="space_8">
+          <Text size="sm" fontWeight="bold" noOfLines={1}>
+            {cookbook.name}
+          </Text>
+          <FavoriteCookbook
+            isSubscribed={cookbook.isSubscribed}
+            cookbookId={cookbook.id}
+          />
+        </Flex>
+
+        <AvatarAndName owner={cookbook.owner} />
+
+        <PrivacyAndVotes
+          isPublic={cookbook.isPublic}
+          upvotes={cookbook.upvotes}
+          downvotes={cookbook.downvotes}
+        />
+
+        <FormattedDate timestamp={cookbook.creationTimestampMs} />
+      </HStack>
+
+      {!cookbook.recipes || cookbook.recipes.length === 0 ? (
+        <ViewCookbookSnippetsEmpty />
+      ) : (
+        <SnippetResults results={cookbook.recipes} />
+      )}
+    </Box>
+  );
+}
