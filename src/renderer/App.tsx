f1afc43edcfcaf6433254a079d64c6d2a46c8078
Daniel <daniel@code-inspector.com>
2022-09-09 08:31:22 -0700

    feat: initial rollbar setup

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -2,6 +2,7 @@ import { ApolloProvider } from '@apollo/client';
 import { ChakraProvider, ColorModeScript } from '@chakra-ui/react';
 import { theme } from '@codiga/components';
 import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
+import { Provider as RollbarProvider, ErrorBoundary } from '@rollbar/react';
 
 // PAGES
 import Home from './pages/Home';
@@ -21,6 +22,7 @@ import './styles/app.css';
 
 // OTHER
 import client from './graphql/client';
+import { rollbarConfig } from './lib/rollbar';
 import Layout from './components/Layout';
 import Filters from './components/Filters/Filters';
 import { UserProvider } from './components/UserContext';
@@ -29,49 +31,59 @@ import { FiltersProvider } from './components/FiltersContext';
 
 export default function App() {
   return (
-    <ApolloProvider client={client}>
-      <ColorModeScript initialColorMode="system" />
-      <ChakraProvider theme={theme}>
-        <UserProvider>
-          <ThemeProvider>
-            <FiltersProvider>
-              <Router>
-                <Layout>
-                  <Routes>
-                    <Route element={<Filters />}>
-                      <Route path="/" element={<Home />} />
-                      <Route path="/my-snippets" element={<MySnippets />} />
-                      <Route
-                        path="/favorite-snippets"
-                        element={<FavoriteSnippets />}
-                      />
-                      <Route path="/my-cookbooks" element={<MyCookbooks />} />
-                      <Route
-                        path="/favorite-cookbooks"
-                        element={<FavoriteCookbooks />}
-                      />
-                      <Route path="/team-snippets" element={<TeamSnippets />} />
-                      <Route
-                        path="/team-cookbooks"
-                        element={<TeamCookbooks />}
-                      />
-                    </Route>
+    <RollbarProvider config={rollbarConfig}>
+      <ErrorBoundary>
+        <ApolloProvider client={client}>
+          <ColorModeScript initialColorMode="system" />
+          <ChakraProvider theme={theme}>
+            <UserProvider>
+              <ThemeProvider>
+                <FiltersProvider>
+                  <Router>
+                    <Layout>
+                      <Routes>
+                        <Route element={<Filters />}>
+                          <Route path="/" element={<Home />} />
+                          <Route path="/my-snippets" element={<MySnippets />} />
+                          <Route
+                            path="/favorite-snippets"
+                            element={<FavoriteSnippets />}
+                          />
+                          <Route
+                            path="/my-cookbooks"
+                            element={<MyCookbooks />}
+                          />
+                          <Route
+                            path="/favorite-cookbooks"
+                            element={<FavoriteCookbooks />}
+                          />
+                          <Route
+                            path="/team-snippets"
+                            element={<TeamSnippets />}
+                          />
+                          <Route
+                            path="/team-cookbooks"
+                            element={<TeamCookbooks />}
+                          />
+                        </Route>
 
-                    <Route
-                      path="/view-snippet/:snippetId"
-                      element={<ViewSnippet />}
-                    />
-                    <Route
-                      path="/view-cookbook/:cookbookId"
-                      element={<ViewCookbookSnippets />}
-                    />
-                  </Routes>
-                </Layout>
-              </Router>
-            </FiltersProvider>
-          </ThemeProvider>
-        </UserProvider>
-      </ChakraProvider>
-    </ApolloProvider>
+                        <Route
+                          path="/view-snippet/:snippetId"
+                          element={<ViewSnippet />}
+                        />
+                        <Route
+                          path="/view-cookbook/:cookbookId"
+                          element={<ViewCookbookSnippets />}
+                        />
+                      </Routes>
+                    </Layout>
+                  </Router>
+                </FiltersProvider>
+              </ThemeProvider>
+            </UserProvider>
+          </ChakraProvider>
+        </ApolloProvider>
+      </ErrorBoundary>
+    </RollbarProvider>
   );
 }
