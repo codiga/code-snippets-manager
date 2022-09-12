1ff1af6f9333e9fc6652a0042e9e7bb0b4df1df2
Daniel <daniel@code-inspector.com>
2022-09-12 12:46:05 -0700

    chore: remove debug_prod flag

--- .erb/configs/webpack.config.main.prod.ts
+++ .erb/configs/webpack.config.main.prod.ts
@@ -56,7 +56,6 @@ const configuration: webpack.Configuration = {
      */
     new webpack.EnvironmentPlugin({
       NODE_ENV: 'production',
-      DEBUG_PROD: true,
       START_MINIMIZED: false,
     }),
 
