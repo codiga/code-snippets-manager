f651aed2f5d5b3b4dc947221a91229d250387a62
dastrong-codiga <100973772+dastrong-codiga@users.noreply.github.com>
2023-01-08 15:48:21 -0800

    chore: test integration

---
+++ src/renderer/components/Test.tsx
@@ -0,0 +1,21 @@
+console.info('dfdf');
+
+const hasEnable = true;
+
+function checkIfUpdated() {
+  return true;
+}
+
+// const isUpdated = checkIfUpdated()
+const isUpdated = () => null;
+
+const hasValid = true;
+const isInvalid = false;
+
+const hasValue = true;
+const isNoValue = true;
+
+const isValid = 'true';
+const mini = false;
+
+const Hello = () => <img src="#" />;
